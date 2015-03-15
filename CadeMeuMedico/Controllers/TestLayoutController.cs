29b4abaa08d99335d58fdc3bd36f4fd18d7c954a
cyro809 <cyro809@gmail.com>
2015-03-15 13:34:57 -0300

    Primeiro Commit - Inicio do Cade Meu Medico

---
+++ CadeMeuMedico/Controllers/TestLayoutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CadeMeuMedico.Controllers
+{
+    public class TestLayoutController : Controller
+    {
+        //
+        // GET: /TestLayout/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
